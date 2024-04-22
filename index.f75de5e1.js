f004767b54172ac5569edba98083f6b12a415bb6
Marta Chobaniuk <marta.chobaniuk@gmail.com>
2024-04-22 13:31:38 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const field = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const y = (field.clientHeight - spider.clientHeight) / 2;
+const x = (field.clientWidth - spider.clientWidth) / 2;
+spider.style.top = `${y}px`;
+spider.style.left = `${x}px`;
+
+//# sourceMappingURL=index.f75de5e1.js.map
